350ad22c955f198ff88402b72ef8930a8a390d3b
aoki <aoki@aoki.dev>
2022-05-17 22:00:38 +0900

    check udp max size

--- src/udp_client.rs
+++ src/udp_client.rs
@@ -15,4 +15,17 @@ pub fn communicate(address: &str) -> Result<(), failure::Error> {
             str::from_utf8(&buffer).expect("failed to convert to String")
         );
     }
+
+    // thread 'main' panicked at 'Failed to send data: Os { code: 40, kind: Uncategorized, message: "Message too long" }', src/udp_client.rs:21:10
+    // note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+    // let input = "a".to_string().repeat(70000);
+
+    // let input = "a".to_string().repeat(65507);
+    // let input = "a".to_string().repeat(9216);
+
+    // socket
+    //     .send_to(input.as_bytes(), &address)
+    //     .expect("Failed to send data");
+
+    Ok(())
 }
